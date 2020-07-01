01971a0e4bed014aa5618a3cdb88ad9d07d81527
leolin9527 <963522074@qq.com>
2020-07-01 18:10:29 +0800

    完成订单功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate'
 import Param from '../components/goods/Param'
 import List from '../components/goods/List'
 import Add from '../components/goods/Add'
+import Order from '../components/order/Order'
 
 Vue.use(VueRouter)
 
@@ -51,6 +52,9 @@ const routes = [
       }, {
         path: '/goods/add',
         component: Add
+      }, {
+        path: '/orders',
+        component: Order
       }
     ]
   }
